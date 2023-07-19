1410af33e1ced89a820896d5cdeff80c5ecef956
shavy <shavymtl@gmail.com>
2023-07-19 12:51:19 -0400

    AllFilesFirstCommit

---
+++ SQLUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+        public static DataTable GetDataTable(string sqlstatement) //GetDataTable, take sql statement and return a DataTable
+        {
+            Debug.Print(sqlstatement);
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+            //change user...
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
