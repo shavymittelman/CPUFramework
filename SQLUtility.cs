debc16396f720fed64282c5b9f3f77f95876572e
shavy <shavymtl@gmail.com>
2023-08-02 19:44:12 -0400

    session 22

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -39,6 +39,7 @@ namespace CPUFramework
                 try
                 {
                     SqlDataReader dr = cmd.ExecuteReader();
+                    CheckReturnValue(cmd);
                     if (loadtable == true) 
                     { 
                     dt.Load(dr);
@@ -57,6 +58,42 @@ namespace CPUFramework
             SetAllColumnsAllowNull(dt);
             return dt;
         }
+
+        private static void CheckReturnValue(SqlCommand cmd)
+        {
+            int returnvalue = 0;
+            string msg = "";
+            if (cmd.CommandType == CommandType.StoredProcedure) 
+            { 
+                foreach (SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction == ParameterDirection.ReturnValue)
+                    {
+                        if (p.Value != null)
+                        {
+                            returnvalue = (int)p.Value;
+                        }
+                    }
+                    else if (p.ParameterName.ToLower() == "@message")
+                    {
+                        if(p.Value != null)
+                        {
+                            msg = p.Value.ToString();
+                        }
+                    }
+                }
+
+                if (returnvalue == 1)
+                {
+                    if (msg == "")
+                    {
+                        msg = $"{cmd.CommandText} did not do action that was requested";
+                    }
+                    throw new Exception(msg);
+                }
+            }
+        }
+
         public static DataTable GetDataTable(string sqlstatement) //GetDataTable, take sql statement and return a DataTable
         {
 
