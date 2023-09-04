13075ef87c2e324f624b82ffbe3555c06f23a9e8
shavy <shavymtl@gmail.com>
2023-09-04 18:46:21 -0400

    session 27 record keeper

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -7,7 +7,27 @@ namespace CPUFramework
 {
     public class SQLUtility
     {
-        public static string ConnectionString = "";
+        private static string ConnectionString = "";
+
+        public static void SetConnectionString(string connstring, bool tryopen, string userid = "", string password = "")
+        {
+            ConnectionString = connstring;
+            if (userid != "")
+            {
+                SqlConnectionStringBuilder b = new();
+                b.ConnectionString = ConnectionString; 
+                b.UserID = userid;
+                b.Password = password;
+                ConnectionString = b.ConnectionString;
+            }
+            if (tryopen)
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                }
+            }
+        }
 
         public static SqlCommand GetSQLCommand(string sprocname)
         {
@@ -286,6 +306,7 @@ namespace CPUFramework
             StringBuilder sb = new StringBuilder();
             if (cmd.Connection != null)
             {
+                //sb.AppendLine($"--{cmd.Connection.ConnectionString}");
                 sb.AppendLine($"--{cmd.Connection.DataSource}");
                 sb.AppendLine($"use {cmd.Connection.Database}");
                 sb.AppendLine("go");
