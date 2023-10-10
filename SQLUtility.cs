9b0f99b003e362e56c63a67ce6b13d2d5b943a9a
shavy <shavymtl@gmail.com>
2023-10-10 17:53:05 -0400

    session 42

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -172,6 +172,7 @@ namespace CPUFramework
 
         public static void SetParamValue(SqlCommand cmd, string paramname, object value)
         {
+            if (paramname.StartsWith("@") == false) { paramname = "@" + paramname; }
             try
             {
                 cmd.Parameters[paramname].Value = value;
